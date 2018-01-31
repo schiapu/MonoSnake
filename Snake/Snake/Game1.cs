23e7d5a962fe3ddb7045e460cdad3f3c76041e34
schiapu <gschiappucci@gmail.com>
2018-01-31 01:08:22 -0300

    Initial Commit. Snake Movement and Eating done.

---
+++ Snake/Snake/Game1.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace Snake
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        int baseMove = 32;
+        int screenHeight;
+        int screenWidth;
+
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        InputHelper inputHelper;
+
+        Snake player;
+        Texture2D snakeTexture;
+        Texture2D appleTexture;
+        Rectangle appleRect;
+        Vector2 movement;
+
+
+        public Game1()
+        {
+            screenHeight = 20 * baseMove;
+            screenWidth = 25 * baseMove;
+
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            this.TargetElapsedTime = TimeSpan.FromSeconds(1.0f / 15f);
+
+            graphics.PreferredBackBufferWidth = screenWidth;  // set this value to the desired width of your window
+            graphics.PreferredBackBufferHeight = screenHeight;   // set this value to the desired height of your window
+            graphics.ApplyChanges();
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            StartGame();
+            inputHelper = new InputHelper();
+        }
+
+        public void StartGame()
+        {
+            base.Initialize();
+
+            player = new Snake(snakeTexture, baseMove);
+            appleRect = new Rectangle(64, 64, appleTexture.Width, appleTexture.Height);
+            movement = new Vector2(1, 0);
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            snakeTexture = Content.Load<Texture2D>("Snake");
+            appleTexture = Content.Load<Texture2D>("apple");
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            inputHelper.Update();
+
+            if (inputHelper.IsNewKeyPress(Keys.Escape)) { Exit(); }
+
+            if (inputHelper.IsNewKeyPress(Keys.Left) && movement.X == 0)
+            {
+                movement.X = -1;
+                movement.Y = 0;
+            }
+            if (inputHelper.IsNewKeyPress(Keys.Right) && movement.X == 0)
+            {
+                movement.X = 1;
+                movement.Y = 0;
+            }
+            if (inputHelper.IsNewKeyPress(Keys.Up) && movement.Y == 0)
+            {
+                movement.X = 0;
+                movement.Y = -1;
+            }
+            if (inputHelper.IsNewKeyPress(Keys.Down) && movement.Y == 0)
+            {
+                movement.X = 0;
+                movement.Y = 1;
+            }
+            
+            player.Update(movement, graphics, appleRect);
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.ForestGreen);
+
+            spriteBatch.Begin();
+            player.Draw(spriteBatch);
+            spriteBatch.Draw(appleTexture, appleRect, Color.White);
+            spriteBatch.End();
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Snake/Snake/Snake.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Snake
+{
+    public class Snake
+    {
+        Texture2D texture;
+        Rectangle head;
+        List<Rectangle> body;
+        int baseMove;
+
+        public Snake(Texture2D texture, int baseMove)
+        {
+            this.texture = texture;
+            this.baseMove = baseMove;
+            int x = baseMove * 12;
+            int y = baseMove * 9;
+            this.head = new Rectangle(x, y, texture.Width, texture.Height);
+            body = new List<Rectangle>
+            {
+                new Rectangle(x - baseMove,y - baseMove, texture.Width, texture.Height),
+                new Rectangle(x - baseMove * 2,y - baseMove * 2, texture.Width, texture.Height)
+            };
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, head, Color.White);
+            foreach (Rectangle bodyPart in body)
+            {
+                spriteBatch.Draw(texture, bodyPart, Color.White);
+            }            
+        }
+
+        public void Update(Vector2 movement, GraphicsDeviceManager graphics, Rectangle appleRect)
+        {
+            Rectangle oldPos = head;
+            Rectangle newPos = oldPos;
+
+            //Head movement
+            newPos.X += (int)(movement.X * baseMove);
+            newPos.Y += (int)(movement.Y * baseMove);
+
+            //Wrapping screen
+            if (newPos.X + 1 > graphics.PreferredBackBufferWidth)
+            {
+                newPos.X = 0;
+            }
+            if (newPos.X < 0)
+            {
+                newPos.X = graphics.PreferredBackBufferWidth - baseMove;
+            }
+            if (newPos.Y + 1 > graphics.PreferredBackBufferHeight)
+            {
+                newPos.Y = 0;
+            }
+            if (newPos.Y < 0)
+            {
+                newPos.Y = graphics.PreferredBackBufferHeight - baseMove;
+            }
+            head = newPos;
+
+            //Body follow up
+            for (int i = 0; i < body.Count; i++)
+            {
+                newPos = body[i];
+                body[i] = oldPos;
+                oldPos = newPos;
+
+                //Kill Snake if it intersects with Body
+                if (head.Intersects(body[i]))
+                {
+                    new Game1();
+                    break;
+                }
+            }
+
+            //Eating Apple
+            if (head.Intersects(appleRect))
+            {
+                body.Add(new Rectangle(body[body.Count - 1].X, body[body.Count - 1].Y, texture.Width, texture.Height));
+            }
+        }
+    }
+}
